0fc08133df60dc815916095523ccc3ae285fcf97
McFunston <mica.funston@gmail.com>
2016-12-04 15:26:01 -0500

    Add ColumnWorkUnitMap struct

---
+++ ScheduleEntities/ColumnWorkUnitMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleEntities
+{
+    struct ColumnWorkUnitMap
+    {
+        public int JobNumber { get; set; }
+        public int ClientName { get; set; }
+        public int TaskName { get; set; }
+        public int StartTime { get; set; }
+        public int Duration { get; set; }
+        public int Quantity { get; set; }
+        public int IsReady { get; set; }
+    }
+}
