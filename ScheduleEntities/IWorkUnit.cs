0eeadb4ea998d499b780013ad24e4929afa63993
McFunston <mica.funston@gmail.com>
2016-12-03 20:03:29 -0500

    Add WorkUnit and IWorkUnit

---
+++ ScheduleEntities/IWorkUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ScheduleEntities
+{
+    interface IWorkUnit
+    {
+        string ClientName { get; set; }
+        float Duration { get; set; }
+        bool IsReady { get; set; }
+        string JobNumber { get; set; }
+        int Quantity { get; set; }
+        DateTime StartTime { get; set; }
+        string TaskName { get; set; }
+    }
+}
---
+++ ScheduleEntities/WorkUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleEntities
+{
+    class WorkUnit : IWorkUnit
+    {
+        public string JobNumber { get; set; }
+        public string ClientName { get; set; }
+        public string TaskName { get; set; }
+        public DateTime StartTime { get; set; }
+        public float Duration { get; set; }
+        public int Quantity { get; set; }
+        public bool IsReady { get; set; }
+    }
+}
